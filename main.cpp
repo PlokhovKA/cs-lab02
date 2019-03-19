69565ad28bf4920b09e5219479a1e7a3c61f64b0
Alice (PlokhovKA) <kirill15072000@gmail.com>
2019-03-19 16:37:22 +0300

    code: дабликат

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int a, b;
+    double a, b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
